2a9166e0a397be9e27549793b2bf5bcb8276f4d5
deepsmaganti <deepsmaganti@gmail.com>
2020-03-22 12:28:13 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
-python3.7 ./src/consumer_complaints.py ./input/consumer_complaints.csv ./output/report.csv
+python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
