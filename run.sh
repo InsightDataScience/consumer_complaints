361d857c09a35456b51a42e2578e1d193743f327
deepsmaganti <deepsmaganti@gmail.com>
2020-03-22 16:15:32 -0700

    New test data

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
-python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
+#python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
+python3.7 ./src/consumer_complaints.py ./insight_testsuite/tests/test_dataset/input/complaints.csv ./insight_testsuite/tests/test_dataset/output/report.csv
